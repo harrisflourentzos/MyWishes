11d5a7727ec36e9c5d2e40647991f284cd238ff4
harrisflourentzos <lrentzo227@gmail.com>
2017-06-19 21:16:49 +0200

    Initial Commit activity_main.xml works activity_wish_list.xml works

---
+++ app/src/main/java/mywishes/androidstudio/com/mywishes/MainActivity.java
@@ -0,0 +1,29 @@
+package mywishes.androidstudio.com.mywishes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        saveButton = (Button) findViewById(R.id.buttonSaveId);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, WishList.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/mywishes/androidstudio/com/mywishes/WishList.java
@@ -0,0 +1,13 @@
+package mywishes.androidstudio.com.mywishes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WishList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish_list);
+    }
+}
