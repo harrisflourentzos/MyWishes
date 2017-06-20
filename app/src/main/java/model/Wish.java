fbde631b136e0f14aa7d1f50c91ec0e2d4f093a6
harrisflourentzos <lrentzo227@gmail.com>
2017-06-20 17:08:44 +0200

    Classes: Constants DatabaseHandler Wish

---
+++ app/src/main/java/model/Wish.java
@@ -0,0 +1,36 @@
+package model;
+
+/**
+ * Created by harri on 20/06/2017.
+ */
+
+public class Wish {
+
+    public String title;
+    public String content;
+    public String recordDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(String recordDate) {
+        this.recordDate = recordDate;
+    }
+}
