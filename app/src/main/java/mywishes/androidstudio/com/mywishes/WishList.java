29937ef8518c56202761b2be8cb1d43224bb5872
harrisflourentzos <lrentzo227@gmail.com>
2017-06-20 19:18:07 +0200

    add wish_list_template.xml start populating WishList.java

--- app/src/main/java/mywishes/androidstudio/com/mywishes/WishList.java
+++ app/src/main/java/mywishes/androidstudio/com/mywishes/WishList.java
@@ -1,13 +1,70 @@
 package mywishes.androidstudio.com.mywishes;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
-public class WishList extends AppCompatActivity {
+import java.util.ArrayList;
+
+import data.DatabaseHandler;
+import model.Wish;
+
+public class WishList extends Activity {
+    private DatabaseHandler dba;
+    private ArrayList<Wish> dbWishes = new ArrayList<>();
+    private WishAdapter wishAdapter;
+    private ListView listView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_list);
+
+        listView = (ListView) findViewById(R.id.listViewId);
+
+        refreshData();
+
+
+
+    }
+
+    private void refreshData() {
+        dbWishes.clear();
+        dba = new DatabaseHandler(getApplicationContext());
+
+        ArrayList<Wish> wishesFromDB = dba.getWishes();
+
+        for (int i = 0; i < wishesFromDB.size(); i++){
+
+            String title = wishesFromDB.get(i).getTitle();
+            String dateText = wishesFromDB.get(i).getRecordDate();
+            String content = wishesFromDB.get(i).getContent();
+//            int mid = wishesFromDB.get(i).getItemId();
+
+
+//            Log.v("IDs: " , String.valueOf(mid));
+
+            Wish myWish = new Wish();
+            myWish.setTitle(title);
+            myWish.setContent(content);
+            myWish.setRecordDate(dateText);
+//            myWish.setItemId(mid);
+
+
+
+            dbWishes.add(myWish);
+
+
+        }
+        dba.close();
+
+    }
+
+    private class WishAdapter {
     }
 }
